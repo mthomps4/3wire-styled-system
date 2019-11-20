157aa00359a4f19b6cb802cf1e9f054da888686b
Matt Thompson <matt@echobind.com>
2019-11-20 16:55:54 -0500

    bump

--- build/components/forms/Form.js
+++ build/components/forms/Form.js
@@ -2,12 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const styled_1 = require("@emotion/styled");
 const styled_system_1 = require("styled-system");
-const applyBoolStyles = ({ centerContent, flex }) => {
+const applyBoolStyles = ({ centerContent }) => {
     let styles = '';
     if (centerContent)
         styles = styles + 'display: flex; justify-content: center; align-items: center;';
-    if (flex)
-        styles = styles + 'display: flex;';
     return styles;
 };
 const Form = styled_1.default('form')(applyBoolStyles, styled_system_1.compose(styled_system_1.space, styled_system_1.layout, styled_system_1.color, styled_system_1.background, styled_system_1.border, styled_system_1.position, styled_system_1.flexbox, styled_system_1.grid));
