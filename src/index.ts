217f83f2fb6af90fc31d66a8c15b72109fad4c15
Matt Thompson <matt@echobind.com>
2019-11-25 09:19:51 -0500

    forgot to export new components -- bump

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'emotion-theming';
+import Button from './components/forms/Button';
 import Form from './components/forms/Form';
 import Label from './components/forms/Label';
 import TextInput from './components/forms/TextInput';
+import Card from './components/layout/Card';
 import Column from './components/layout/Column';
 import Container from './components/layout/Container';
 import Row from './components/layout/Row';
@@ -9,4 +11,4 @@ import Spacer from './components/layout/Spacer';
 import Text from './components/Text';
 import defaultTheme from './defaultTheme';
 
-export { Container, Row, Column, Spacer, Form, TextInput, Label, Text, ThemeProvider, defaultTheme };
+export { Button, Card, Container, Row, Column, Spacer, Form, TextInput, Label, Text, ThemeProvider, defaultTheme };
